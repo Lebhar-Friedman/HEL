0972a22a6884424680d13291c657e21664d046e4
Muhammad Asif <asif@maavan.com>
2017-06-13 14:46:59 +0500

    yii2 advanced framework setup - auto increment-> sugnup

---
+++ common/models/Counter.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace common\models;
+
+use Yii;
+use yii\base\NotSupportedException;
+use yii\behaviors\TimestampBehavior;
+use yii\mongodb\ActiveRecord;
+use yii\web\IdentityInterface;
+
+/**
+ * Counter model
+ *
+ * @property integer $id
+ * @property string $counter_name
+ * @property string $sequence_value
+ * @property integer $created_at
+ * @property integer $updated_at
+ * @property string $password write-only password
+ */
+class Counter extends ActiveRecord {
+    const COUNTER_USER_ID = 'userid';
+
+    /**
+     * @inheritdoc
+     */
+    public static function collectionName() {
+        return ['health_events', 'counter'];
+    }
+
+    //setup for model attributes
+    public function attributes() {
+        return ['_id',
+            'counter_name', // name of counter sequence related to sepecific model
+            'sequence_value', // to save currunt incremented sequence id
+            'created_at',
+            'updated_at',
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules() {
+        return [
+            [['_id',
+            'counter_name', // name of counter sequence related to sepecific model
+            'sequence_value', // to save currunt incremented sequence id
+            'created_at',
+            'updated_at'], 'safe'],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function behaviors() {
+        return [
+            'timestamp' => [
+                'class' => 'yii\behaviors\TimestampBehavior',
+                'attributes' => [
+                    ActiveRecord::EVENT_BEFORE_INSERT => ['created_at', 'updated_at'],
+                    ActiveRecord::EVENT_BEFORE_UPDATE => ['updated_at'],
+                ],
+            ]
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public static function findIdentity($id) {
+        return static::findOne(['_id' => $id]);
+    }
+
+    public static function getAutoIncrementId($counterName) {
+        $counter = static::findOne(['counter_name' => $counterName]);
+        if ($counter) {
+            $counter->sequence_value++;
+        } else {
+            $counter = new Counter();
+            $counter->counter_name = $counterName;
+            $counter->sequence_value = 1;
+        }
+        $counter->save();
+        return $counter->sequence_value;
+    }
+
+// end class counter
+}
