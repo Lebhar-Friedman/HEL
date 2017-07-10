5f877875338768b22a4057c76bb7b64062863efe
zeeshan <zeeshan@AAI>
2017-07-10 19:22:32 +0500

    company search and pagination

--- backend/views/company/index.php
+++ backend/views/company/index.php
@@ -1,17 +1,19 @@
 <?php
 
+use backend\components\CustomLinkPager;
+
 use yii\helpers\BaseUrl;
 use yii\helpers\Url;
 use yii\widgets\LinkPager;
 ?>
-
+<?php $this->title='Companies'; ?>
 <div class="col-lg-12">
     <div class="search-comp-content">
         <div class="company-container">
             <div class="col-lg-2"></div>
             <center>
                 <div class="row col-lg-8">
-                    <form method="get" action="<?= BaseUrl::base().'/company' ?>">
+                    <form method="get" action="<?= BaseUrl::base() . '/company' ?>">
                         <input type="text" name="name" class="search-box search-img" value="" placeholder="Search Term" />
                     </form>
                 </div>
@@ -19,7 +21,7 @@ use yii\widgets\LinkPager;
 
             <div class="row ">
                 <div class="col-lg-6 col-md-4 col-sm-4">
-                    <div class="flt-lft  add-new-btn2"><a href="<?= BaseUrl::base().'/company/detail'; ?>">Add New</a></div>
+                    <div class="flt-lft  add-new-btn2"><a href="<?= BaseUrl::base() . '/company/detail'; ?>">Add New</a></div>
                 </div>
                 <div class="col-lg-6 col-md-8 col-sm-8">
                     <div class="total">Total: <?= $total; ?></div>
@@ -38,33 +40,26 @@ use yii\widgets\LinkPager;
                     if ($company['logo'] === null || $company['logo'] === '' || !isset($company['logo'])) {
                         $logo_url = Url::to('@web/images/upload-logo.png');
                     } else {
-                        $logo_url = Url::to('@web/uploads/'. $company['logo']);
+                        $logo_url = Url::to('@web/uploads/' . $company['logo']);
                     }
                     ?>
                     <div class="main-table">
                         <div class="tc1"><img src="<?= $logo_url ?>" alt=""  width="60px" /></div>
                         <div class="tc2"><?= $company['name'] ?></div>
                         <div class="tc2-2">26</div>
-                        <div class="tc3">26<img src="<?= Url::to('@web/images/caution.png') ?>" alt="" /></div>
-                        <div class="tc4"><a href="#" class="del-btn "></a><a href="<?=  BaseUrl::base().'/company/detail?cid='.$company['_id']?>" class="edit-btn "></a></div>
+                        <!--<div class="tc3">26<img src="<?= Url::to('@web/images/caution.png') ?>" alt="" /></div>-->
+                        <div class="tc4"><a href="#" class="del-btn "></a><a href="<?= BaseUrl::base() . '/company/detail?cid=' . $company['_id'] ?>" class="edit-btn "></a></div>
                     </div>
                 <?php } ?>
-                <div class="main-table">
-                    <div class="tc1"><img src="images/wallmart.png" alt="" /></div>
-                    <div class="tc2">Wallmart Pharmacy</div>
-                    <div class="tc2-2">11</div>
-                    <div class="tc5">11</div>
-                    <div class="tc4"><a href="#" class="del-btn "></a><a href="#" class="edit-btn "></a></div>
-                </div>
             </div>
 
-        </div>
-    </div>
-</div>
 <?php
 if (isset($pagination)) {
-    echo LinkPager::widget([
+    echo CustomLinkPager::widget([
         'pagination' => $pagination,
     ]);
 }
 ?>
+        </div>
+    </div>
+</div>
