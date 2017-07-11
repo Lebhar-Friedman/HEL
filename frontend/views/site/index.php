b03e454df439d273b4a8561f11cd15d22d56e718
zeeshan <zeeshan@AAI>
2017-07-11 12:21:40 +0500

    frontend: home page

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -1,16 +1,84 @@
 <?php
 
-/* @var $this yii\web\View */
-
+$baseUrl = Yii::$app->request->baseUrl;
 $this->title = 'Health Events';
 ?>
-<div class="site-index">
+<div class="company-content">
+    <div class="container">
+        <i>Find health events from companies like</i>
+        <div>
+            <img src="<?= $baseUrl ?>/images/event-img1.png" alt="" />
+            <img src="<?= $baseUrl ?>/images/event-img2.png" alt="" />
+            <img src="<?= $baseUrl ?>/images/event-img3.png" alt="" />
+            <img src="<?= $baseUrl ?>/images/event-img4.png" alt="" />
+            <img src="<?= $baseUrl ?>/images/event-img5.png" alt="" />
+            <img src="<?= $baseUrl ?>/images/event-img6.png" alt="" />
+            <img src="<?= $baseUrl ?>/images/event-img7.png" alt="" />
+        </div>
+    </div>
+</div>
+<div class="container">
+    <h1 class="health-h">Health comes to your local store</h1>
+    <div class="row">
+        <div class="col-lg-4 col-md-4 col-sm-4">
+            <div class="health-box">
+                <span><img src="<?= $baseUrl ?>/images/search-img.png" alt="" /></span>
+                Search for health <br />services near you
+            </div>
+        </div>
+        <div class="col-lg-4 col-md-4 col-sm-4">
+            <div class="health-box">
+                <span><img src="<?= $baseUrl ?>/images/timmer.png" alt="" /></span>
+                Save time <br />& money
+            </div>
+        </div>
+        <div class="col-lg-4 col-md-4 col-sm-4">
+            <div class="health-box">
+                <span><img src="<?= $baseUrl ?>/images/heart.png" alt="" class="margin-t" /></span>
+                Keep your whole <br />family healthy!
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-lg-6 col-md-6 col-sm-6">
+            <div class="consider-text">
+                <span>Consider the convenience of being able to get a health screening on a weekend or during a lunch break, when you don’t necessarily have to worry about racing back to your job. Consider, too, that you can get your blood pressure tested at the same place you buy your groceries. Or, you can check up on your glucose levels while picking up a prescription. Two birds, one stone, no extra trips.</span>
+            </div>
+        </div>
+        <div class="col-lg-6 col-md-6 col-sm-6">
+            <div class="consider-text">
+                Common health services are now more accessible and affordable than ever, and that means you don’t have to feel pressured to put them off any longer. But how do you find out which retailer is holding what health event when, especially when so many different places are participating?
 
-    <div class="body-content">
-
-        <div class="row">
-            
+                <div class="margin-t">That’s where Health Events Live can help!</div>
+            </div>
         </div>
 
+    </div>
+    <h1 class="from-h">From allergies to osteoperosis</h1>
+    <div class="row">
+        <div class="col-lg-7 col-md-7 col-sm-7">
+            <div class="health-care-text">
+                <span>Health care is experiencing a huge shift, bringing clinical services to your local retailer.</span>
+
+                <span>Drug stores, grocery stores, big box stores and even membership clubs now offer a wide range of health screenings, checkups and consultations, many of which are free.</span>
+
+                <span>HealthEventsLive.com was developed to provide health-conscious consumers with comprehensive listings of every free and low-cost health service available at retail stores, hospitals, community centers and more — all in one place.
+                </span>
+            </div>
+        </div>
+        <div class="col-lg-5 col-md-5 col-sm-5">
+            <div class="debate-txt">
+                <span>Diabetes-related screenings</span>
+                <span>Heart health events</span>
+                <span>Cholesterol</span>
+                <span>Nutrition</span>
+                <span>Osteoporosis</span>
+                <span>Vaccinations and immunizations</span>
+                <span>Vision and hearing</span>
+                <span>Senior health</span>
+                <span>And much more!</span>
+                <a href="#"><img src="<?= $baseUrl ?>/images/search-btn.png" alt="" />Find events near me!</a>
+            </div>
+        </div>
     </div>
 </div>
