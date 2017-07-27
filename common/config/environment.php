b52d848f559092265d946b299f9e647626d2902a
zeeshan <zeeshan@AAI>
2017-07-27 11:55:35 +0500

    dev settings for company logo

--- common/config/environment.php
+++ common/config/environment.php
@@ -3,5 +3,5 @@ $host = filter_var($_SERVER['HTTP_HOST'], FILTER_SANITIZE_STRING);
 if (strstr($host, "localhost")) {
     define("ENV", "local");
 }else{
-    define("ENV", "prod");
+    define("ENV", "dev");
 }
