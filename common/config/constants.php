f5445aa8fbfe455d682c51413d1333171e61d31d
zeeshan <zeeshan@AAI>
2017-08-07 21:10:35 +0500

    broken images setting

--- common/config/constants.php
+++ common/config/constants.php
@@ -5,7 +5,7 @@ include 'environment.php';
 if (ENV === "local") {
     define('IMG_URL', 'http://localhost/HEL/backend/web/uploads/');
 } else if (ENV === "dev") {
-    define('IMG_URL', 'http://13.59.81.62/HEL/frontend/web/uploads/');
+    define('IMG_URL', 'http://13.59.81.62/HEL/backend/web/uploads/');
 } else {
     define('IMG_URL', 'Will be updated soon');
 }
