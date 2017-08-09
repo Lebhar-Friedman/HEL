b3fafb1b9083a033bd87c31096001221f8ec77a3
Muhammad Asif <asif@maavan.com>
2017-08-09 19:15:32 +0500

    frontend:  provider profile

--- frontend/controllers/UserController.php
+++ frontend/controllers/UserController.php
@@ -10,27 +10,59 @@ namespace frontend\controllers;
 
 use common\functions\GlobalFunctions;
 use yii\web\Controller;
+use yii\filters\AccessControl;
 
 /**
  * Description of UserController
  *
  * @author zeeshan
  */
-class UserController extends Controller{
+class UserController extends Controller {
+
+    /**
+     * @inheritdoc
+     */
+    public function behaviors() {
+        return [
+            'access' => [
+                'class' => AccessControl::className(),
+                'rules' => [
+                    [
+                        'allow' => true,
+                        'roles' => ['@'],
+                    ],
+                ],
+            ],
+        ];
+    }
+
     public function actionIndex() {
         
     }
-    
+
     public function actionProfile() {
-        return $this->render("profile");
+        $logoLinks = $events = [];
+        $events = \common\models\Event::findAll(['_id' => \Yii::$app->user->identity->saved_events]);
+
+        foreach ($events as $event) {
+            $company = \common\models\Company::findOne(['name' => $event->company]);
+            if (count($company) > 0 && !empty($company->logo)) {
+                $logoLinks[$event->event_id] = IMG_URL . $company['logo'];
+            } else {
+                $logoLinks[$event->event_id] = \yii\helpers\BaseUrl::base() . '/images/upload-logo.png';
+            }
+        }
+
+        return $this->render("profile", ['events' => $events, 'companyLogo' => $logoLinks]);
     }
-    
+
     public function actionAlerts() {
-        $keywords=GlobalFunctions::getKeywords();
-        $alerts_list= array();
-        foreach ($keywords as $keyword){
+        $keywords = GlobalFunctions::getKeywords();
+        $alerts_list = array();
+        foreach ($keywords as $keyword) {
             array_push($alerts_list, $keyword['text']);
         }
-        return $this->render("alerts",['alerts_list' => $alerts_list]);
+        return $this->render("alerts", ['alerts_list' => $alerts_list]);
     }
+
 }
