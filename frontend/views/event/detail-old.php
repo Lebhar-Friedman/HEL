97f42b5fe7d398b2f43026c576da316545959b25
Sajid <Sajid@WINDOWS-7R6A84M>
2017-08-11 20:29:29 +0500

    Merge origin/master
    Conflicts:
    	frontend/views/event/detail.php

--- frontend/views/event/detail-old.php
+++ frontend/views/event/detail-old.php
@@ -38,21 +38,26 @@ if ($coordinates = GlobalFunctions::getCookiesOfLngLat()) {
     }
 </style>
 <?php $img_url = BaseUrl::base() . '/images/'; ?>
-
+<?php if($error !== ''){
+?>
+<p class="text-center"><b><?=$error?></b></p>   
+<?php
+}else{
+?>
 <div class="container">
-    <div class="row">
+    	<div class="row">
 <!--            <div class="col-lg-1 col-md-2 col-sm-2">
                 <a href="javascript:location.replace(document.referrer);" class="back-btn">&lt; Back</a>
             </div>
         	<div class="col-lg-1"></div>
             div class="col-lg-7 col-md-8 col-sm-8 col-xs-7">-->
             <div class="col-lg-9 col-md-10 col-sm-8 col-xs-7">
-            <div class="senior-day-content">
-                <h1><?= $event['title'] ?></h1>
-                <h2><?= GlobalFunction::getEventDate($event['date_start'], $event['date_end']) ?></h2>
-                <?= $event['time_start'] ?> - <?= $event['time_end'] ?> 
-                <div class="save-share-btn clearfix">
-                    <?php
+            	<div class="senior-day-content">
+                	<h1><?=$event['title']?></h1>
+                        <h2><?= GlobalFunction::getEventDate($event['date_start'], $event['date_end']) ?></h2>
+                    <?=$event['time_start']?> - <?=$event['time_end']?> 
+                    <div class="save-share-btn clearfix">
+                        <?php
                     if (isset(Yii::$app->user->identity->_id)) {
                         ?>
                         <a href="javascript:;" onclick="saveEvent('<?= $event['_id'] ?>', this)"><img src="<?= $img_url ?>star-icon.png" alt="" /> SAVE</a>
@@ -66,40 +71,32 @@ if ($coordinates = GlobalFunctions::getCookiesOfLngLat()) {
 
                     <div class="addthis_inline_share_toolbox"></div>
                 </div>
-
-                <div class="clearfix">
-                    <?php
-                    foreach ($event['categories'] as $category):
-                        ?>
-                        <div class="heart-text"><?= $category ?></div>
-                        <?php
-                    endforeach;
-                    ?>
+            </div>
+            <div class="col-lg-3 col-md-4 col-sm-4 col-xs-5">
+                <div class="cvs-text">
+                    <img src="<?=GlobalFunctions::getCompanyLogo($company['name'])?>" alt="" />
+                	<div class="margin-t">
+                        <?=$company['street']?><br />
+                        <?=$company['state']?>, <?=$company['zip']?><br />
+                        <?=$company['phone']?><br />
+                    </div>
+                    <?php 
+                    
+                    if(sizeof($event['locations'])> 1){ echo "<span>More locations nearby</span>";}?>
+                    <a href="#map">Show map</a>
                 </div>
             </div>
         </div>
-        <div class="col-lg-3 col-md-4 col-sm-4 col-xs-5">
-            <div class="cvs-text">
-                <img src="<?= GlobalFunctions::getCompanyLogo($company['name']) ?>" alt="" />
-                <div class="margin-t">
-                    <?= $company['street'] ?><br />
-                    <?= $company['state'] ?>, <?= $company['zip'] ?><br />
-                    <?= $company['phone'] ?><br />
-                </div>
-               <?php if(sizeof($event['locations'])> 1){ echo "<span>More locations nearby</span>";}?>
-                    <a href="#map">Show map</a>            </div>
-        </div>
-    </div>
-    <div class="row">
+        <div class="row">
 <!--        	<div class="col-lg-1"></div>
                 <div class="col-lg-10">-->
             <div class="col-lg-12">
-            <div class="free-health-content">
-                <h1>FREE Healthcare Services</h1>
-                <h2>No appointment required!</h2>
-                <div class="row">
-                    <?php
-                    foreach ($event['sub_categories'] as $sub_category):
+            	<div class="free-health-content">
+                	<h1>FREE Healthcare Services</h1>
+                    <h2>No appointment required!</h2>
+                    <div class="row">
+                    	<?php
+                        foreach ($event['sub_categories'] as $sub_category):
                         ?>
                         <div class="col-lg-3 col-md-3 col-sm-3 col-xs-3">
                             <i><?= $sub_category ?></i>
@@ -110,32 +107,31 @@ if ($coordinates = GlobalFunctions::getCookiesOfLngLat()) {
                 </div>
             </div>
         </div>
-    </div>
-    <div class="event-detail-img show-on-mobile"><img src="<?= $img_url ?>result-img7.png" alt="" /></div>
-    <div class="row">
+        <div class="event-detail-img show-on-mobile"><img src="<?= $img_url ?>result-img7.png" alt="" /></div>
+        <div class="row">
 <!--        	<div class="col-lg-1"></div>
             <div class="col-lg-6 col-md-8 col-sm-8">-->
             <div class="col-lg-8 col-md-9 col-sm-8">
-            <div class="event-detail-text">
-                <h1>Health event details</h1>
-                <?= $event['description'] ?>
+            	<div class="event-detail-text">
+                	<h1>Health event details</h1>
+                	<?=$event['description']?>
+                </div>
             </div>
-        </div>
-        <div class="col-lg-4 col-md-4 col-sm-4">
-            <div class="event-detail-img hide-on-mobile"><img src="<?= $img_url ?>result-img7.png" alt="" /></div>
-        </div>
-    </div>
-    <div class="row">
+            <div class="col-lg-4 col-md-4 col-sm-4">
+            	<div class="event-detail-img hide-on-mobile"><img src="<?= $img_url ?>result-img7.png" alt="" /></div>
+            </div>
+      	</div>
+        <div class="row">
 <!--        	<div class="col-lg-1"></div>
             <div class="col-lg-10">-->
             <div class="col-lg-12">
-            <div class="map2-content">
-                <h1><?php if(sizeof($event['locations'])> 1){ echo "Locations";}else{echo "Location";}?> for this event</h1>
+            	<div class="map2-content">
+                	<h1><?php if(sizeof($event['locations'])> 1){ echo "Locations";}else{echo "Location";}?> for this event</h1>
 <!--                    <img src="<?= $img_url ?>map-img.png" alt="" />-->
-                <?php if (sizeof($event) > 0) { ?>
-                    <div class="map-content">
-                            <!--<a href="javascript:;" onclick='openModal(<?php echo json_encode($event); ?>)' class="view-all-btn" style="z-index: 99">View all event locations</a>-->
-                        <?php
+                    <?php if (sizeof($event) > 0) { ?>
+    <div class="map-content" id="map">
+            <!--<a href="javascript:;" onclick='openModal(<?php echo json_encode($event); ?>)' class="view-all-btn" style="z-index: 99">View all event locations</a>-->
+            <?php
 //            $coord = new LatLng(['lat' => 32.154377, 'lng' => 74.184227]);
                         $coord = new LatLng(['lat' => intval($user_lat), 'lng' => intval($user_lng)]);
                         $map = new Map([
@@ -166,21 +162,25 @@ if ($coordinates = GlobalFunctions::getCookiesOfLngLat()) {
 //                $map->appendScript("google.maps.event.addListenerOnce(gmap, 'idle', function(){
 //            google.maps.event.trigger(abc, 'click');});");
 
-                            $map->addOverlay($marker);
-                        }
+                    $map->addOverlay($marker);
+                }
+            
+            $map->center = $map->getMarkersCenterCoordinates();
+            $map->zoom = $map->getMarkersFittingZoom() - 1;
 
-                        $map->center = $map->getMarkersCenterCoordinates();
-                        $map->zoom = $map->getMarkersFittingZoom() - 1;
-
-                        echo $map->display();
-                        ?>
-                    </div>
-                <?php } ?>
+            echo $map->display();
+            ?>
+        </div>
+    <?php } ?>
+                </div>
             </div>
         </div>
-    </div>
+        <div class="row">
+<!--        	<div class="col-lg-1"></div>
+          	<div class="col-lg-7 col-md-8 col-sm-8">-->
+
     <div class="row">
-<!--        <div class="col-lg-1"></div>-->
+        <div class="col-lg-1"></div>
         <div class="col-lg-7 col-md-8 col-sm-8">
             <?php
             if (!empty($companyEvents)) {
@@ -227,13 +227,34 @@ if ($coordinates = GlobalFunctions::getCookiesOfLngLat()) {
                     ?>
                 </div>
                 <?php
-            }
-            ?>
-            <div class="email-content">
-                <div class="row">
-                    <div class="col-lg-11 col-md-12">
-                        <h1>Alert me when more health events added at this location!</h1>
-                        <div class="show-on-mobile">Alert me when healthcare events are added at CVS Pharmacy, 503 Wonder Lane</div>
+                }
+                ?>
+                <div class="email-content">
+                	
+                	<div class="row">
+                    	<div class="col-lg-11 col-md-12">
+                        	<h1>Alert me when more health events added at this location!</h1>
+                            <div class="show-on-mobile">Alert me when healthcare events are added at CVS Pharmacy, 503 Wonder Lane</div>
+                        </div>
+                    </div>
+                    <div class="row">
+                    	<div class="col-lg-8 col-md-10">
+                        	
+                            <div class="email-conatiner">
+                            	<input type="text" class="email-textbox" placeholder="Email" />
+                                <input type="submit" value="Go" class="submitbtn" />
+                            </div>
+                        </div>
+                    </div>
+                	
+                </div>
+            </div>
+            <div class="col-lg-offset-2 col-lg-3 col-md-4 col-sm-4">
+            	<div class="cvs-text mobile-center">
+                	<img src="<?=GlobalFunctions::getCompanyLogo($company['name'])?>" alt="" />
+                	<div class="find-out-text">
+                    	<!--<img src="<?= $img_url ?>result-detail-img2.png" alt="" />-->
+                            <a href="<?= \yii\helpers\Url::to(['/provider', 'id' => $company['name']]); ?>">Find out more <br class="hide-on-mobile" />about <?=$company['name']?> <br class="hide-on-mobile" /></a>
                     </div>
                 </div>
                 <div class="row">
@@ -248,23 +269,26 @@ if ($coordinates = GlobalFunctions::getCookiesOfLngLat()) {
 
             </div>
         </div>
-        <div class="col-lg-offset-2 col-lg-3 col-md-4 col-sm-4">
+        <div class="col-lg-3 col-md-4 col-sm-4">
             <div class="cvs-text mobile-center">
                 <img src="<?= GlobalFunctions::getCompanyLogo($company['name']) ?>" alt="" />
                 <div class="find-out-text">
                 <!--<img src="<?= $img_url ?>result-detail-img2.png" alt="" />-->
-                           <a href="<?= \yii\helpers\Url::to(['/provider', 'id' => $company['name']]); ?>">Find out more <br class="hide-on-mobile" />about <?=$company['name']?> <br class="hide-on-mobile" /></a>
-                    </div>
+                    <a href="<?= \yii\helpers\Url::to(['/provider', 'id' => $company['name']]); ?>"><h2>Find out more <br class="hide-on-mobile" />about CVS <br class="hide-on-mobile" />Pharmacies</h2></a>
+                </div>
             </div>
         </div>
     </div> 
 </div>
 
 <script type="text/javascript">
-    var addthis_share = {
-        url: "<?= Yii::$app->request->absoluteUrl ?>",
-        title: "<?= $event['title'] ?>",
-        description: "<?= $event['description'] ?>",
-        media: "<?= Yii::$app->urlManager->hostInfo . GlobalFunctions::getCompanyLogo($company['name']) ?>"
-    }
+var addthis_share = {
+   url: "<?= Yii::$app->request->absoluteUrl?>",
+   title: "<?=$event['title']?>",
+   description: "<?=$event['description']?>",
+   media: "<?=Yii::$app->urlManager->hostInfo.GlobalFunctions::getCompanyLogo($company['name'])?>"
+}
 </script>
+<?php
+}
+?>
