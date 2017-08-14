57513094a02e5581778535ddeaeb2e6be93318de
zeeshan <zeeshan@AAI>
2017-08-14 20:00:02 +0500

    bug fixed terms and privacy pages.

--- frontend/views/site/privacy.php
+++ frontend/views/site/privacy.php
@@ -1,28 +1,28 @@
 <?php $this->title = 'Health Events Live: Privacy'; ?>
 <div class="container privacy-container">
     <h1>Privacy</h1>
-    <h3>Why sell your miles</h3>
+    <h3>Lorem Ipsom</h3>
     <p>
         Apart from cashing out and being paid same day, here are some other reasons:<br>
         1. You have accumulated miles but are unable to use them for air tickets when you want.
         2. You don't fly frequently.
         3. Your miles are about to expire.
     </p>
-    <h3>Why sell your miles</h3>
+    <h3>Lorem Ipsom</h3>
     <p>
         Apart from cashing out and being paid same day, here are some other reasons:<br>
         1. You have accumulated miles but are unable to use them for air tickets when you want.
         2. You don't fly frequently.
         3. Your miles are about to expire.
     </p>
-    <h3>Why sell your miles</h3>
+    <h3>Lorem Ipsom</h3>
     <p>
         Apart from cashing out and being paid same day, here are some other reasons:<br>
         1. You have accumulated miles but are unable to use them for air tickets when you want.
         2. You don't fly frequently.
         3. Your miles are about to expire.
     </p>
-    <h3>Why sell your miles</h3>
+    <h3>Lorem Ipsom</h3>
     <p>
         Apart from cashing out and being paid same day, here are some other reasons:<br>
         1. You have accumulated miles but are unable to use them for air tickets when you want.
