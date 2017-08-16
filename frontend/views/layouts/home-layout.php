c087d9a6ceec1ef084c416de708eca8f50ec1a13
Sajid <Sajid@WINDOWS-7R6A84M>
2017-08-16 15:15:48 +0500

    Merge origin/master
    Conflicts:
    	frontend/views/layouts/home-layout.php
    	frontend/views/layouts/main.php

--- frontend/views/layouts/home-layout.php
+++ frontend/views/layouts/home-layout.php
@@ -80,7 +80,7 @@ $baseUrl = Yii::$app->request->baseUrl;
                     <div class="col-lg-8 col-md-8 col-sm-8">
                         <div class="footer-right-side">
 <!--                            <a href="<?= yii\helpers\BaseUrl::base() . '/event/directory' ?>">Directory</a> &bull;     -->
-                            <a href="#">Sitemap</a>     
+                            <!--<a href="#">Sitemap</a>-->     
                             <a href="<?= yii\helpers\BaseUrl::base() . '/site/terms-privacy' ?>">Terms & Privacy</a>
                             <span></span>
                             © Health Events Live.  All rights reserved.
