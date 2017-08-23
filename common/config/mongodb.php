ae0b308e1eb28b05bfc8c8dbadcf53dd614f3e37
zeeshan1853 <zeeshan1853@gmail.com>
2017-08-23 17:11:27 +0500

    connection string

--- common/config/mongodb.php
+++ common/config/mongodb.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => '\yii\mongodb\Connection',
-//    'dsn' => 'mongodb://jemiuser:jemi@user@localhost:27017/health_events',
-    'dsn' => 'mongodb://localhost:27017/health_events',
+    'dsn' => 'mongodb://jemiuser:jemi@user@localhost:27017/health_events',
+//    'dsn' => 'mongodb://localhost:27017/health_events',
     'defaultDatabaseName' => 'health_events',
 ];
