fe32f54f12be1819a7665756f7e005cf65b9477c
zeeshan1853 <zeeshan1853@gmail.com>
2017-08-28 21:14:20 +0500

    more events heading color change.

--- frontend/views/event/_more-events.php
+++ frontend/views/event/_more-events.php
@@ -4,14 +4,19 @@ use common\functions\GlobalFunctions;
 use components\GlobalFunction;
 use yii\helpers\BaseUrl;
 ?>
+<style>
+    h1{
+        color: #333;
+    }
+</style>
 <?php $img_url = BaseUrl::base() . '/images/'; ?>
 <div id="more_events">
+    <div class="event-near">
+        <h1>More health events</h1>
+    </div>
     <?php if (isset($more_events) && sizeof($more_events) > 0) { ?>
         <?php foreach ($more_events as $event) { ?>
             <a href="<?= BaseUrl::base() . '/event/detail?eid=' . (string) $event['_id'] ?>">
-                <div class="event-near">
-                    <h1>More health events</h1>
-                </div>
                 <div class="multi-service">
                     <h1><?= (isset($event['sub_categories']) && sizeof($event['sub_categories']) === 1 ) ? $event['sub_categories'][0] . ' Screenings' : 'Multiple Services' ?></h1>
                     <h2><?= GlobalFunction::getEventDate($event['date_start'], $event['date_end']) ?></h2>
