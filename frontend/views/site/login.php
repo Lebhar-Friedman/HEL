367b3e164137322069bc8f19811adbbf3ac1f929
Muhammad Asif <asif@maavan.com>
2017-09-06 17:16:33 +0500

    backend: upload csv geoLocation null fixes

--- frontend/views/site/login.php
+++ frontend/views/site/login.php
@@ -31,7 +31,7 @@ if (isset($_GET['email'])) {
                 <div class="login-form">
                     <h1>Log In</h1>
                     <div class="row">
-                        <div class="col-lg-4 col-md-1 col-sm-1"> </div>
+                        <div class="col-lg-4 col-md-4 col-sm-4"> </div>
                         <!--                        <div class="col-lg-4 col-md-4 col-sm-4 text-center">
                                                     <div class="left-colm">
                                                         <span>Sign In with your Facebook or Google+ account</span>
