20dbaa417caa241f0a728e216d34cf5d4ccda5ad
Muhammad Asif <asif@maavan.com>
2017-09-08 13:22:16 +0500

    env setting for maavan.org

--- common/config/environment.php
+++ common/config/environment.php
@@ -5,7 +5,7 @@ if (strstr($host, "localhost")) {
     define("ENV", "local");
 } else if (strstr($host, '13.59.81.62')) {
     define("ENV", "dev");
-}else if (strstr($host, 'naseersays.com') || strstr($host, 'healtheventslive.com')) {
+}else if (strstr($host, 'naseersays.com') || strstr($host, 'healtheventslive.com') || strstr($host, 'maavan.org')) {
     define("ENV", "live");
 } else {
     define("ENV", "prod");
