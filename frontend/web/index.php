f897e6b1ca72c0df269d9424162861661468c07a
Muhammad Asif <asif@maavan.com>
2017-09-09 10:10:12 +0500

    frontend: disabled debug mode for live

--- frontend/web/index.php
+++ frontend/web/index.php
@@ -1,6 +1,9 @@
 <?php
-defined('YII_DEBUG') or define('YII_DEBUG', true);
-defined('YII_ENV') or define('YII_ENV', 'dev');
+
+if ($_SERVER['SERVER_NAME'] != 'www.healtheventslive.com' || $_SERVER['SERVER_NAME'] != 'www.alumniconvos.net' || $_SERVER['SERVER_NAME'] != '13.59.81.62') {
+    defined('YII_DEBUG') or define('YII_DEBUG', true);
+    defined('YII_ENV') or define('YII_ENV', 'dev');
+}
 
 require(__DIR__ . '/../../vendor/autoload.php');
 require(__DIR__ . '/../../vendor/yiisoft/yii2/Yii.php');
