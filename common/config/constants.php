251c5765f13b0f2f197a70eb0e8930df48e89aca
zeeshan1853 <zeeshan1853@gmail.com>
2017-10-02 20:13:09 +0500

    Meta Tag updated and backup API key

--- common/config/constants.php
+++ common/config/constants.php
@@ -19,7 +19,7 @@ if (ENV === "local") {
 } else if (ENV === "live") {
     define('IMG_URL', 'http://13.59.81.62/HEL/backend/web/uploads/');
     define('MONGODB_DSN', 'mongodb://jemiuser:jemi#user@localhost:27017/health_events');
-     define('GOOGLE_API_URL', 'https://maps.googleapis.com/maps/api/geocode/json?key='.GOOGLE_API_KEY_BACKUP.'&');
+     define('GOOGLE_API_URL', 'https://maps.googleapis.com/maps/api/geocode/json?key='.GOOGLE_API_KEY.'&');
 } else {
     define('IMG_URL', 'http://13.59.81.62/HEL/backend/web/uploads/');
     define('MONGODB_DSN', 'mongodb://jemiuser:jemi#user@localhost:27017/health_events');
