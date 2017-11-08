a3de581f71896c95640d4d5989f0a9fc68b8ee38
zeeshan1853 <zeeshan1853@gmail.com>
2017-11-08 11:11:32 +0500

    implementing WYSIWYG

---
+++ frontend/vendor/yiisoft/extensions.php
@@ -0,0 +1,15 @@
+<?php
+
+$vendorDir = dirname(__DIR__);
+
+return array (
+  '2amigos/yii2-ckeditor-widget' => 
+  array (
+    'name' => '2amigos/yii2-ckeditor-widget',
+    'version' => '2.1.0.0',
+    'alias' => 
+    array (
+      '@dosamigos/ckeditor' => $vendorDir . '/2amigos/yii2-ckeditor-widget/src',
+    ),
+  ),
+);
