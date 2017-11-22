d3332a29ab1643b1ed61f2a9d3ea166fa39f99e4
zeeshan <zeeshan@maavan-lenovo>
2017-11-22 11:02:15 +0500

    welcome email updated

--- common/mail/registration-welcome-html.php
+++ common/mail/registration-welcome-html.php
@@ -11,13 +11,17 @@ use yii\helpers\Html;
     .password-reset p{
         padding-left: 20px !important; 
         padding-right: 20px !important;
+        text-align: left !important;
+    }
+    .password-reset h1{
+        padding-left: 20px !important; 
+        padding-right: 20px !important;
+        text-align: left !important;
     }
 </style>
 <div class="password-reset" style="padding-left: 20px;padding-right: 20px;">
-    <!--<p>Hello <?/= Html::encode($user->username) ?>,</p>-->
-    <p style="padding-left: 20px;padding-right: 20px;">Welcome to Health Events Live!</p>
+    <h1 style="padding-left: 20px;padding-right: 20px;">Welcome to Health Events Live!</h1>
     <p>Thank you for signing up — a new world of free and low-cost health care is waiting for you.</p>
     <p>Login anytime and go to "My Account” to create email alerts for upcoming events near you. That why you’ll never miss out on valuable health services coming your way. You can also save event details for later, add events to your calendar, and more.</p>
     <p>It’s time to take your health into your own hands with more access to care, greater convenience, and less cost!</p>
-    <!--<p><?/= Html::a(Html::encode($confirmLink), $confirmLink) ?></p>-->
 </div>
